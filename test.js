2b9573ec3d2252021e15541d510046a10294a428
Tony Mottaz <mail@tonymottaz.com>
2023-11-26 20:07:55 -0600

    test all prettier versions 3.0.0 -- 3.1.0

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 import test from "node:test";
 import assert from "node:assert";
-import prettier from "prettier";
 
-const format = async (code) => {
+const allPrettierVersions = await Promise.all([
+  import("prettier-3.0.0"),
+  import("prettier-3.0.1"),
+  import("prettier-3.0.2"),
+  import("prettier-3.0.3"),
+  import("prettier-3.1.0"),
+]);
+
+const format = async (prettier, code) => {
   const output = await prettier.format(code, {
     parser: "html",
     plugins: ["./prettier-plugin-void-html.js"],
@@ -28,47 +35,55 @@ const allVoidElements = [
   "wbr",
 ];
 
-test("preserve void syntax on all void elements", async () => {
-  const results = await Promise.all(
-    allVoidElements.map((el) => format(`<${el}>`)),
-  );
-  results.forEach((formatted, index) => {
-    assert.equal(formatted, `<${allVoidElements[index]}>\n`);
+allPrettierVersions.forEach((dep) => {
+  const { default: prettier, version } = dep;
+
+  test(`Prettier version ${version}`, async (t) => {
+    await t.test("preserve void syntax on all void elements", async () => {
+      const results = await Promise.all(
+        allVoidElements.map((el) => format(prettier, `<${el}>`)),
+      );
+      results.forEach((formatted, index) => {
+        assert.equal(formatted, `<${allVoidElements[index]}>\n`);
+      });
+    });
+
+    await t.test("avoid self-closing syntax on empty elements", async (t) => {
+      await t.test("<div></div>", async () => {
+        const formatted = await format(prettier, `<div></div>`);
+        assert.equal(formatted, `<div></div>\n`);
+      });
+    });
+
+    await t.test("Undo invalid self-closing syntax", async (t) => {
+      await t.test("input", async () => {
+        const formatted = await format(prettier, `<input name="test" />`);
+        assert.equal(formatted, `<input name="test">\n`);
+      });
+
+      await t.test("div", async () => {
+        const formatted = await format(prettier, `<div />`);
+        assert.equal(formatted, `<div></div>\n`);
+      });
+    });
+
+    await t.test("preserve self-closing in SVG", async () => {
+      const formatted = await format(
+        prettier,
+        `<svg><circle cx="50" cy="50" r="50" /></svg>`,
+      );
+      assert.equal(formatted, `<svg><circle cx="50" cy="50" r="50" /></svg>\n`);
+    });
+
+    await t.test("preserve self-closing in MathML", async () => {
+      const formatted = await format(
+        prettier,
+        `<math><mspace depth="40px" height="20px" width="100px" /></math>`,
+      );
+      assert.equal(
+        formatted,
+        `<math><mspace depth="40px" height="20px" width="100px" /></math>\n`,
+      );
+    });
   });
-});
-
-test("avoid self-closing syntax on empty elements", async (t) => {
-  await t.test("<div></div>", async () => {
-    const formatted = await format(`<div></div>`);
-    assert.equal(formatted, `<div></div>\n`);
-  });
-});
-
-test("Undo invalid self-closing syntax", async (t) => {
-  await t.test("input", async () => {
-    const formatted = await format(`<input name="test" />`);
-    assert.equal(formatted, `<input name="test">\n`);
-  });
-
-  await t.test("div", async () => {
-    const formatted = await format(`<div />`);
-    assert.equal(formatted, `<div></div>\n`);
-  });
-});
-
-test("preserve self-closing in SVG", async () => {
-  const formatted = await format(
-    `<svg><circle cx="50" cy="50" r="50" /></svg>`,
-  );
-  assert.equal(formatted, `<svg><circle cx="50" cy="50" r="50" /></svg>\n`);
-});
-
-test("preserve self-closing in MathML", async () => {
-  const formatted = await format(
-    `<math><mspace depth="40px" height="20px" width="100px" /></math>`,
-  );
-  assert.equal(
-    formatted,
-    `<math><mspace depth="40px" height="20px" width="100px" /></math>\n`,
-  );
 });
