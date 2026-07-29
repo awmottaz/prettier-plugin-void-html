94f91b6f9fe9227e02f5cd23f7ec513f7b555b64
Tony Mottaz <mail@tonymottaz.com>
2026-07-29 08:31:21 -0500

    upgrade eslint to v10

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-plugin-prettier/recommended";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
